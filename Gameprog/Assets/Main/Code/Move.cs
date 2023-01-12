d609d404746551629fa40df08b2fe62433e7db59
Pan-koplan <obeyoranother@gmail.com>
2023-01-12 23:39:07 +0300

    Design_setActive

--- Gameprog/Assets/Main/Code/Move.cs
+++ Gameprog/Assets/Main/Code/Move.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
     Vector3 right = new Vector3(1, 0, 0);
     Vector3 up = new Vector3(0, 1, 0);
+    public Sprite t0;
+    public Sprite t1;
+    public Sprite t2;
+    public Sprite t3;
+    public SpriteRenderer gg;
+    
 
     public int dir = 0;//направление, 0 - вниз, 1 - вправо, 2 - вверх, 3 - влево
 
@@ -20,8 +27,6 @@ public class Move : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += right * 0.1f;
@@ -42,5 +47,12 @@ public class Move : MonoBehaviour
             transform.position -= up * 0.1f;
             dir = 0;
         }
+
+        //направление
+        if(dir == 0) { gg.sprite = t0; }
+        if (dir == 1) { gg.sprite = t1; }
+        if (dir == 2) { gg.sprite = t2; }
+        if (dir == 3) { gg.sprite = t3; }
+
     }
 }
