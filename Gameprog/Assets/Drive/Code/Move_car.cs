2c2a9c6d1225a953084c143e3882f2b5111fba33
Pan-koplan <obeyoranother@gmail.com>
2023-01-12 22:03:00 +0300

    Stopometr_ver1

--- Gameprog/Assets/Drive/Code/Move_car.cs
+++ Gameprog/Assets/Drive/Code/Move_car.cs
@@ -10,12 +10,6 @@ using static UnityEngine.GraphicsBuffer;
 [ExecuteAlways]
 public class Move_car : MonoBehaviour
 {
-    //timer
-    int sec = 0;
-    int min = 0;
-    Text timerText;
-    public GameObject timer;
-    public static bool flow = true;
 
     public Transform P0;
     public Transform P1;
@@ -37,10 +31,8 @@ public class Move_car : MonoBehaviour
     int circ = 0;
     public int circ_finish = 15;
 
-    void Awake()
+    void Start()
     {
-        timerText = timer.GetComponent<Text>();
-        StartCoroutine(TimeFlow());
         load.fillAmount = 0;
     }
     void FixedUpdate()
@@ -105,22 +97,6 @@ public class Move_car : MonoBehaviour
             preveousePoint = point;
         }
 
-    }
-
-    IEnumerator TimeFlow()
-    {
-        while (flow)
-        {
-            if (sec == 59)
-            {
-                Debug.Log("1");
-                min++;
-                sec = -1;
-            }
-            sec += 1;
-            timerText.text = min.ToString("D2") + " : " + sec.ToString("D2");
-            yield return new WaitForSeconds(1);
-        }
     }
 
 
